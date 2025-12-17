7bb7f5dd148d10cd8f18fd2aeb17592d57d03a2c
Nada <magdynadamagdy32@email.com>
2025-12-17 21:31:41 +0200

    test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/api", verifyCodeRoute);
 app.use('/api/auth', authRoutes);
 
 app.get('/api/test', (req, res) => {
-  res.send('API is running...');
+  res.json({ message: 'API is running...' });
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
