51ca5ea008fb45b0ff4d01b822d8538d54bd6745
Nada <magdynadamagdy32@email.com>
2025-12-19 15:18:22 +0200

    edit

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -2,7 +2,7 @@ const db = require('../config/db');
 
 exports.register = (req, res) => {
     const { id, name, email, password } = req.body;
-    const checkQuery = `SELECT * FROM user WHERE id = ?`;
+    const checkQuery = `SELECT * FROM person WHERE id = ?`;
     db.query(checkQuery, [id], (err, result) => {
         if (err){ 
             return res.status(500).json({ 
@@ -15,7 +15,7 @@ exports.register = (req, res) => {
                 message: 'User with this ID already exists' 
             });
         }
-        const insertQuery = `INSERT INTO user (id, name, email, password) VALUES (?, ?, ?, ?)`;
+        const insertQuery = `INSERT INTO person (id, name, email, password) VALUES (?, ?, ?, ?)`;
         db.query(insertQuery, [id, name, email, password], (err2, result2) => {
             if (err2){ 
                 return res.status(500).json({ 
@@ -32,7 +32,7 @@ exports.register = (req, res) => {
 exports.login = (req, res) => {
   const { email, password } = req.body;
 
-  const query = `SELECT * FROM user WHERE email = ? AND password = ?`;
+  const query = `SELECT * FROM person WHERE email = ? AND password = ?`;
   db.query(query, [email, password], (err, result) => {
     if (err) return res.status(500).json({ message: 'Database error', error: err });
 
