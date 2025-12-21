b7324d16931ad3f1392546fad88ac0489f404628
Nada <magdynadamagdy32@email.com>
2025-12-21 20:15:38 +0200

    edit

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./config/db');
 const authRoutes = require('./routes/auth');
-const sendCodeRoute = require("./routes/sendCode");
-const verifyCodeRoute = require("./routes/verifyCode");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api", sendCodeRoute.router);
-app.use("/api", verifyCodeRoute);
 
 app.use('/api/auth', authRoutes);
 
