8ac7888b07de3ffcdaa120b1900cd8556b0ed4fc
Nada <magdynadamagdy32@email.com>
2026-01-08 18:00:35 +0200

    edit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API is running...' });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
